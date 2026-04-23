f028c385db84a8ac9a077f19a3c912a39ad42932
ookatuk <hgfhgfojgljgjhdiopyfgiiiiiu@outlook.com>
2026-04-23 13:41:13 +0900

    fix build.rs
    Signed-off-by: ookatuk <hgfhgfojgljgjhdiopyfgiiiiiu@outlook.com>

--- iart-core/build.rs
+++ iart-core/build.rs
@@ -1,4 +1,7 @@
 fn main() {
-    let trace_max = std::env::var("WIRT_TRACE_MAX").unwrap_or_else(|_| "1024".to_string());
-    println!("cargo:rustc-env=BACK_TRACE_MAX={}", trace_max);
+    let trace_max = std::env::var("IART_TRACE_MAX").unwrap_or_else(|_| "1024".to_string());
+    let trace_type = std::env::var("IART_TRACE_TYPE").unwrap_or_else(|_| "good".to_string());
+
+    println!("cargo:rustc-env=IART_TRACE_MAX={}", trace_max);
+    println!("cargo:rustc-env=IART_TRACE_TYPE={}", trace_type);
 }
